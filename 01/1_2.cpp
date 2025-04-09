1a60404aae1420e90fdb78f2eaf67928f69310d4
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-04-09 18:35:19 +0545

    studied about variable and datatype in cpp

---
+++ 01/1_2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  // int
+   int Age = 20;
+
+  //  cout << sizeof(Age)<<endl;  
+
+  // cout <<"I am :" << Age << endl;
+
+  // declare the variable
+  //  int age;
+  //  cout << age;
+
+  // defining the variable
+  // int Age=20;
+  // cout << Age<<endl;
+
+  // Age=21; // Re-assignment can be done but not Re-declaration 
+  // cout<<Age;
+
+  //    --------------------
+
+  float salary = 40000.17675;
+  cout << salary << endl;
+  // cout<<sizeof(salary)<<endl;
+
+  char alphabet = 'a';
+  // cout << alphabet << endl;
+  // cout<<sizeof(alphabet)<<endl;
+
+  double weight = 55.23427857;
+  cout << weight << endl;
+  // cout<<sizeof(weight)<<endl;
+
+  bool isLoggedIn = true;
+  // cout<<sizeof(isLoggedIn)<<endl;
+
+  // cout << isLoggedIn << endl; // 1
+
+
+
+  return 0;
+}
