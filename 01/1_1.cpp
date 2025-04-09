0510d2554cd164be1d1176c71d411382d0a170a7
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-04-09 13:06:20 +0545

    priting to the console done

---
+++ 01/1_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){   // starting point of program
+
+  cout << "Hello and Welcome to the DSA with CPP" << endl;
+
+  cout << "Hello and Welcome to the DSA with CPP\n" ;
+
+  cout << "Hello and Welcome to the DSA with CPP" ;
+
+  return -1;  // successfull execution, other than 0 indicate          unseccessfull execution
+}  
