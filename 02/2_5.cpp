652a633a56b597e454390a22e0f0f68e9c69a1f7
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-04-10 12:49:25 +0545

    some experiments done

---
+++ 02/2_5.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  // int i = -1;
+  // do
+  // {
+  //   cout << i << endl;
+  //   i++;
+  // } while (i <= 5);
+  // int i=1;
+  //   do
+  //   {
+  //     cout<<i<<"Ritesh Kafle"<<endl;
+  //     i++;
+  //   } while (i<=20);
+
+  // for (int i = 1; i <= 5; i++); // This semicolon ends the for loop immediately
+  // {
+  //   cout << i << endl;
+  // }
+
+  // int i = 1;
+  // while (i <= 5);
+
+  // {
+
+  //   cout << i << endl;
+  //   i++;
+  // }
+
+  // while(true);
+
+
+  // int i ;
+  // if(cin >> i){
+  //   cout<<"Ritesh Kafle";
+  // }
+
+  
+  // if(cout << "hi"){
+  //   cout<<"Ritesh Kafle";
+  // }
+
+
+  return 0;
+}
