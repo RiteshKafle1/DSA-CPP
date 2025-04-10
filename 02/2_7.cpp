e61517ba2247dcf6a7be3307f586fe228151b43b
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-04-10 14:58:20 +0545

    some of the operators

---
+++ 02/2_7.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a = 5;
+
+  // cout<<a<<endl;
+  // 5
+  // cout<<++a<<endl;
+  // 6
+  // cout<<a; //6
+
+  // cout << a << endl;
+  // // 5
+  // cout << a++ << endl;
+  // // 5
+  // cout << a << endl;
+  // // 6
+  // cout << a++ << endl;
+  // // 6
+  // cout << a << endl;
+  // 7
+
+  // cout<<5/2<<endl;
+
+  // cout<<5.31/2<<endl;
+
+  // cout<<5.31786/2<<endl;
+
+  // cout<<(5&4)<<endl;
+
+  // cout<<(5|4)<<endl;
+
+  // cout<<(~1)<<endl;
+
+  // cout<<(5<<1)<<endl;
+
+  // cout<<(10>>1)<<endl;
+
+  // cout<<(5^5)<<endl;
+
+  // cout<<(8^8)<<endl;
+
+  // cout<<(8^9)<<endl;
+
+  return 0;
+}
