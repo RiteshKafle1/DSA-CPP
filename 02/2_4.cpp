49c03678e366f4408a99ea8535c49989e5467623
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-04-10 11:33:41 +0545

    questions of forloop done

---
+++ 02/2_4.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // int i = 1;
+  // while (i <= 10)
+  // {
+  //   cout << i << endl;
+  //   i++;
+  // }
+
+  // for (int i = 1; i <= 100; i++)
+  // {
+  //   cout << i << " ";
+
+  // }
+  // for (int i = 100; i >= 1; i--)
+  // {
+  //   cout << i << " ";
+  // }
+
+  // for (int i = 1; i < 51; i++)
+  // {
+  //   cout << i << "Ritesh" << endl;
+  // }
+
+  // for (int i = 0; i >= -10; i--)
+
+  // {
+  //   cout << i << endl;
+  // }
+
+  // for (int i = 1; i <= 10; i++)
+  // {
+  //   cout << 7*i << endl;
+
+  // }
+  // for (char i = 'A'; i <=  'Z'; i++)
+  // {
+  //   cout << i << endl;  // internally works using ASCII code.
+  // }
+
+  // for (char i = 'a'; i <= 'z'; i++)
+  // {
+  //   cout << i << " ";
+  // }
+
+  return 0;
+}
