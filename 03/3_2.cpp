ac703f6c1c4e2de110f8e4b37b83b517b4feea1a
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-04-11 13:10:10 +0545

    something about type-conversion.

---
+++ 03/3_2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+
+int main()
+{ // int to float
+  // int number1 = 10;
+  // float number2 = 2.45;
+
+  // // int result=number1+number2; //this would oviously run but not the correct way.
+  // float result = number1 + number2; //correct way.
+  // cout << result << endl;
+
+  // char to int
+
+  // char ch = 'A';
+  // int numberOne = 1;
+
+  // int result = ch + numberOne;
+  // // char result1 = ch + numberOne;
+  // cout << result << endl;
+  // // cout << result1 << endl;
+
+  // double pi=3.14159265;
+  // // int intPi=(int)pi;
+  // cout << intPi;
+
+  // float floatingNumber = 90.89;
+
+  // char charValue = char(floatingNumber);
+
+  // cout << charValue << endl;
+
+
+
+  int a=10;
+  int b=3.8;
+
+  cout<<(float)b;
+
+  // float c=a/(float(b));
+  // cout<<c;
+
+
+  return 0;
+}
