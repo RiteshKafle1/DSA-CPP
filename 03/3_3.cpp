9a28f69d44a45cdd37064087d72426ec88f6bd88
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-04-11 20:44:03 +0545

    hollow rectangle and hollow square

--- 03/3_3.cpp
+++ 03/3_3.cpp
@@ -1,29 +1,75 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
   // square pattern
   // for (int i = 0; i <= 3; i++)
   // {
   //   for (int j = 0; j <= 3; j++)
   //   {
-  //     cout<<"*"; 
+  //     cout<<"*";
   //   }
   //   cout<<endl;
-     
+
   // }
 
-  //rectangle pattern
-  // for (int i = 0; i <3; i++)
+  // rectangle pattern
+  //  for (int i = 0; i <3; i++)
+  //  {
+  //    for (int j = 0; j <= 4; j++)
+  //    {
+  //      cout<<"*";
+  //    }
+  //    cout<<endl;
+
+  // }
+
+  //Hollow Rectangle
+
+  // for (int i = 0; i <= 3; i++)
   // {
   //   for (int j = 0; j <= 4; j++)
   //   {
-  //     cout<<"*"; 
+  //     if (i == 0 || i == 3)
+  //     {
+  //       cout << "*";
+  //     }
+  //     else if(i==1 || i==2){
+  //       if(j==0 || j==4){
+  //         cout <<"*";
+  //       }
+  //       else{
+  //         cout<<" ";
+  //       }
+  //     }
+
   //   }
-  //   cout<<endl;
-     
+  //     cout << endl;
   // }
-  
-  
+
+    // Hollow Square
+    // for (int i = 0; i <= 4 ; i++)
+    // {
+    //     for (int j = 0; j <= 4; j++)
+    //     {
+    //       if(i==0||i==4){
+    //         cout<<"*";
+    //       }
+    //       else if(i==1 || i==2 || i==3 ){
+    //         if(j==0 || j==4){
+    //           cout<<"*";
+    //         }
+    //         else{
+    //           cout<<" ";
+    //         }
+    //       }
+    //     }
+    //     cout<<endl;
+        
+    // }
+    
+
+
   return 0;
 }
