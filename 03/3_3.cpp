b1ed8f2214e8058c658c276e1b86e02d5a4fd504
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-04-11 20:06:56 +0545

    square and rectangle pattern

---
+++ 03/3_3.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  // square pattern
+  // for (int i = 0; i <= 3; i++)
+  // {
+  //   for (int j = 0; j <= 3; j++)
+  //   {
+  //     cout<<"*"; 
+  //   }
+  //   cout<<endl;
+     
+  // }
+
+  //rectangle pattern
+  // for (int i = 0; i <3; i++)
+  // {
+  //   for (int j = 0; j <= 4; j++)
+  //   {
+  //     cout<<"*"; 
+  //   }
+  //   cout<<endl;
+     
+  // }
+  
+  
+  return 0;
+}
