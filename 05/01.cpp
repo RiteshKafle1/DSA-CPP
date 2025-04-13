9a0f95cf0568405434a1f8cdef78ca9ead8ea8fd
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-04-13 13:29:33 +0545

    more about bitwise operator

---
+++ 05/01.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // cout << (5 & 3)<<endl;
+
+  // cout << (5 | 3);
+  // cout<<"\n"<<~5;
+
+  // cout<<"\n"<<(5^5);
+
+  // cout<<"\n"<<(25<<1);
+
+  // cout<<"\n"<<((-10)<<2);
+
+  // cout<<"\n"<<(10>>1);
+
+  // cout<<"\n"<<((-10)>>1);
+
+  // cout<<"\n"<<((-4)>>1);
+  // if (15&1)
+  // {
+  //   cout<<"Odd Number";
+  // }else{
+  //   cout<<"Even Number";
+  // }
+
+  // int number = 100;
+  // int bitCount = 0;
+
+  // while (number != 0)
+  // {
+  //   int lastBit = (number & 1);
+  //   if (lastBit==1   )
+  //     bitCount += 1;
+  //  number = number >> 1;
+  // }
+  // cout<<bitCount;
+ 
+  return 0;
+}
