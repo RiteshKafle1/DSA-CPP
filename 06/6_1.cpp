fdb791055477319c82a589ea247e702726311380
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-04-14 13:21:51 +0545

    creation of array and fill method.

---
+++ 06/6_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // int arr[10];// creation of array;
+
+  int arr[10] = {1, 2, 3, 4, 5, 6};
+  // cout << arr[6] << endl;
+  // cout << arr[7] << endl;
+  // cout << arr[8] << endl;
+  // cout << arr[9] << endl;
+  // cout << arr[3] << endl;
+  // cout << arr[4] << endl;
+
+    int arr2[3];
+  fill(arr2,arr2+10,10);
+  cout<<arr[0]<<endl;
+  cout<<arr[1]<<endl;
+  cout<<arr[2];
+ 
+  return 0;
+}
