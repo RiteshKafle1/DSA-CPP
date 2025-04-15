24fd4dde29a9c64b3f2aca1fe6b3552453b790a8
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-04-15 18:58:11 +0545

    vector

---
+++ 07/7_7.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+  // vector
+
+     
+
+  // vector<int> v;
+  // v.push_back(1);
+  // v.push_back(2);
+  // v.push_back(3);
+  // // cout << v.size() << endl;
+  // // cout << v.capacity();
+  // v.pop_back();
+  // int size = v.size();
+  
+  // for (int i = 0; i < size; i++)
+  // {
+  //   // cout << v[i] << "  ";
+  //   cout<<v.at(i)<<" ";
+  // }
+  // v.clear();
+  // v.push_back(10);
+ 
+    
+  // for (int i = 0; i < size; i++)
+  // {
+  //   // cout << v[i] << "  ";
+  //   cout<<v.at(i)<<" ";
+  // }
+  
+
+  return 0;
+}
