947a18e8fe6e8c4e84fa325ccdf601e7037e707a
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-04-27 10:29:37 +0545

    right rotation of an array uisng reverse method done.

---
+++ Arrays/05.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void rightRotate(int arr[], int size, int k)
+{
+
+  int rotation = k % size;
+
+  int low = 0;
+  int high = size - 1;
+
+  // reverse an array
+  while (low < high)
+  {
+    swap(arr[low], arr[high]);
+    low++;
+    high--;
+  }
+  // reverse upto rotation.
+
+  low = 0;
+  high = rotation - 1;
+  while (low < high)
+  {
+    swap(arr[low], arr[high]);
+    low++;
+    high--;
+  }
+  //
+  // reverse from rotation to size.
+
+  low = rotation;
+  high = size - 1;
+  while (low < high)
+  {
+    swap(arr[low], arr[high]);
+    low++;
+    high--;
+  }
+}
+
+int main()
+{
+  int arr[] = {1, 2, 3, 4, 5, 6, 7};
+  int size = 7;
+  int rotateBy = 8;
+  rightRotate(arr, size, rotateBy);
+  for (int a : arr)
+    cout << a << "  ";
+  return 0;
+}
