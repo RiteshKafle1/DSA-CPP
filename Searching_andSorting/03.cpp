3dada4c36e6ac1c158ababa7bc34f5bbc842415e
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-04-27 08:01:21 +0545

    finding the single number from the array (method 2)

---
+++ Searching_andSorting/03.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void findSingleNumber(int arr[], int size, int &ansBox)
+{
+  int i = 0;
+  while (i < size - 1)
+  {
+    if (arr[i] == arr[i + 1])
+      i += 2;
+      else
+        break;
+  }
+  ansBox = arr[i];
+}
+int main()
+{
+  int arr[] = {1, 2, 3, 1, 2, 3, 4,4,5};
+  int size = 9;
+  sort(arr, arr + size);
+  int ansBox = -1;
+  findSingleNumber(arr, size, ansBox);
+  cout << ansBox << " ";
+
+  return 0;
+}
