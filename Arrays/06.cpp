f550936128d43d54daa8092132bff209c83c77aa
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-04-28 16:50:11 +0545

    enjoyed solving this problem this problem felt dream.

---
+++ Arrays/06.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+void rotateImage(int arr[][3], int row, int col)
+
+{
+
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = i; j < col; j++)
+    {
+      swap(arr[i][j], arr[j][i]);
+    }
+  }
+  for (int i = 0; i < row;)
+  {
+    int low = 0;
+    int high = row - 1;
+
+    while (low < high)
+    {
+      swap(arr[i][low], arr[i][high]);
+      low++;
+      high--;
+    }
+    i++;
+  }
+}
+
+int main()
+{
+  int arr[3][3] = {
+      {1, 2, 3},
+      {4, 5, 6},
+      {7, 8, 9},
+  };
+  int rowSize = 3;
+  int colSize = 3;
+  rotateImage(arr, rowSize, colSize);
+
+  for (int i = 0; i < rowSize; i++)
+  {
+    for (int j = 0; j < colSize; j++)
+    {
+      cout << arr[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
